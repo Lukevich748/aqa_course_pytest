60ebea91430e070915b5a888f07ce888270f6269
Lukevich <luckevichartem@gmail.com>
2024-07-15 17:33:27 +0200

    parametrize

--- lesson_3_parametrize/test_rest_api.py
+++ lesson_3_parametrize/test_rest_api.py
@@ -17,3 +17,30 @@ class TestAPI:
             url=f"{self.BASE_URL}{endpoint}"
         )
         assert response.status_code == status_code
+
+    @pytest.mark.parametrize(
+        "payload, status_code", [
+            ({"title": "foo", "body": "bar", "id": 1488}, 201),
+            ({}, 201)
+        ]
+    )
+    def test_post_methods(self, payload, status_code):
+        response = requests.post(
+            url=f"{self.BASE_URL}/posts",
+            json=payload
+        )
+        assert response.status_code == status_code
+
+    @pytest.mark.parametrize(
+        "post_id, payload, status_code", [
+            (1, {"id": 1, "title": "foo", "body": "bar", "userId": 1}, 200),
+            (0, {"id": 0, "title": "foo", "body": "bar", "userId": 1}, 500),
+            (1488, {}, 500),
+        ]
+    )
+    def test_put_requests(self, post_id, payload, status_code):
+        response = requests.put(
+            url=f"{self.BASE_URL}/posts/{post_id}",
+            json=payload
+        )
+        assert response.status_code == status_code

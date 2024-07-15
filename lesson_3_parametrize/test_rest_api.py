e0b02652861e709e755400d1810020c6e79a92cf
Lukevich <luckevichartem@gmail.com>
2024-07-15 07:48:17 +0200

    parametrize

--- lesson_3_parametrize/test_rest_api.py
+++ lesson_3_parametrize/test_rest_api.py
@@ -1,3 +1,4 @@
+import pytest
 import requests
 
 
@@ -5,8 +6,14 @@ class TestAPI:
 
     BASE_URL = "https://jsonplaceholder.typicode.com"
 
-    def test_get_methods(self):
+    @pytest.mark.parametrize(
+        "endpoint, status_code", [
+            ("/posts", 200),
+            ("/posts/missing_post", 404)
+        ]
+    )
+    def test_get_methods(self, endpoint, status_code):
         response = requests.get(
-            url=f"{self.BASE_URL}/posts"
+            url=f"{self.BASE_URL}{endpoint}"
         )
-        
+        assert response.status_code == status_code
